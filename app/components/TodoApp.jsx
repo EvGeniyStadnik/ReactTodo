c27e057358b8d3b3e7fc3d09f81f7299785b1e9c
EvGeniyStadnik <evgeniy.stadnik@gmail.com>
2017-01-31 00:28:12 +0200

    create main component TodoApp.jsx

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+class TodoApp extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>TodoApp.js</div>
+        )
+    }
+}
+
+module.exports = TodoApp;
