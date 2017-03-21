e20d72915be8b207cb24e9d86b8c732097f6512f
EvGeniyStadnik <evgeniy.stadnik@gmail.com>
2017-03-21 19:19:33 +0200

    Add auth to action tests

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -34,26 +34,6 @@ describe('Actions', () => {
         expect(res).toEqual(action);
     });
 
-    it('should create todo and dispatch ADD_TODO through startAddTodo()-thunk', (done) => {
-        const store = createMockStore({});
-        const todoText = 'My todo item';
-//we returned promise chain from func() that startAddTodo() returned
-        store.dispatch(actions.startAddTodo(todoText)).then(() => {
-            const actions = store.getActions();
-            expect(actions[0]).toInclude({
-                type: 'ADD_TODO'
-            });
-            expect(actions[0].todo).toInclude({
-                text: todoText
-            });
-            //remove created, during test, todo item
-            let id = actions[0].todo.id;
-            firebaseRef.child(`todos/${id}`).remove();
-
-            done();
-        }).catch(done);
-    });
-
     it('should generate ADD_TODOS action object with todos array', () => {
         let todos = [{
             id: 111,
@@ -96,30 +76,36 @@ describe('Actions', () => {
 
     describe('Tests with firebase todos', () => {
         let testTodoRef;
+        let uid;
+        let todosRef;
 
         beforeEach((done) => {
-            let todosRef = firebaseRef.child('todos');
+            var credential = firebase.auth.GithubAuthProvider.credential(process.env.GITHUB_ACCESS_TOKEN);
 
-            todosRef.remove().then(() => {
-                testTodoRef = firebaseRef.child('todos').push();
+            firebase.auth().signInWithCredential(credential).then((user) => {
+                uid = user.uid;
+                todosRef = firebaseRef.child(`users/${uid}/todos`);
+
+                return todosRef.remove();
+            }, (e) => {console.log(e)}).then(() => {
+                testTodoRef = todosRef.push();
 
                 return testTodoRef.set({
                     text: 'Something to do',
                     completed: false,
-                    createdAt: 123412
-                });
+                    createdAt: 23453453
+                })
             })
             .then(() => done())
             .catch(done);
-
         });
 
         afterEach((done) => {
-            testTodoRef.remove().then(() => done());
+            todosRef.remove().then(() => done());
         });
 
         it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
-            const store = createMockStore({});
+            const store = createMockStore({auth: {uid}});
             const action = actions.startToggleTodo(testTodoRef.key, true);
 
             store.dispatch(action).then(() => {
@@ -139,7 +125,7 @@ describe('Actions', () => {
         });
 
         it('should fetch todos from firebase convert to array and dispatch ADD_TODOS action', (done) => {
-            const store = createMockStore({});
+            const store = createMockStore({auth: {uid}});
             const action = actions.startAddTodos();
 
             store.dispatch(action).then(() => {
@@ -156,6 +142,26 @@ describe('Actions', () => {
 
             }, done);
             done();
+        });
+
+        it('should create todo and dispatch ADD_TODO through startAddTodo()-thunk', (done) => {
+            const store = createMockStore({auth: {uid}});
+            const todoText = 'My todo item';
+    //we returned promise chain from func() that startAddTodo() returned
+            store.dispatch(actions.startAddTodo(todoText)).then(() => {
+                const actions = store.getActions();
+                expect(actions[0]).toInclude({
+                    type: 'ADD_TODO'
+                });
+                expect(actions[0].todo).toInclude({
+                    text: todoText
+                });
+                //remove created, during test, todo item
+                let id = actions[0].todo.id;
+                firebaseRef.child(`todos/${id}`).remove();
+
+                done();
+            }).catch(done);
         });
     });
 
